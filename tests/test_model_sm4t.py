cd2db6bb973e96f205211d9bbd0412be18cfeff9
Benjamin-Pong <156998337+Benjamin-Pong@users.noreply.github.com>
2026-06-10 18:14:16 -0700

    added test for unsupported languages - functionality for sliding window inference regime

---
+++ tests/test_model_sm4t.py
@@ -0,0 +1,5 @@
+from model_sm4t import SchedulerPerLayer, SeamlessM4TConformerEncoderDynamicMasking,  DynamicSeamlessM4T #use absolute import because this file is outside the directory of model_sm4t
+import pytest
+
+
+
