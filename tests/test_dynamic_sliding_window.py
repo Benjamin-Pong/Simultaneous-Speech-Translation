933b8f89de7dc4fc63149b518b04b0e7fc838a18
Benjamin-Pong <156998337+Benjamin-Pong@users.noreply.github.com>
2026-06-19 15:40:31 -0700

    created dummy simulstream package paths to import .model_sm4t.py

--- tests/test_dynamic_sliding_window.py
+++ tests/test_dynamic_sliding_window.py
@@ -1,5 +1,5 @@
 import pytest
-from dynamic_sliding_window import DynamicSlidingWindow
+from simulstream.server.speech_processors.dynamic_sliding_window import DynamicSlidingWindow
 '''
 Test suite to test the functionality of DynamicSlidingWindow inference regime
 '''
